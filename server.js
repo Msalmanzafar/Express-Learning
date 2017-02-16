23ad135be59d90138e7172959d2330aaa1117974
msalmanzafar <m.salman.zafar292@gmail.com>
2017-02-16 16:06:35 +0500

    experss

--- server.js
+++ server.js
@@ -1,19 +1,80 @@
-var express = require("express");
+// var express = require("express");
+// var mongoose = require("mongoose");
 
 
+// var app = express();
+
+// var port = (process.env.PORT || 3000);
+
+// app.get("/", function(req, res, next){
+//     console.log("request is comming to '/");
+//     res.send("hello Word");
+// });
+// app.get("/home", function(req, res, next){
+//     console.log("request home is comming to '/");
+//     res.send("this is my home page..");
+// });
+// mongoose.connect("mongodb://salman:2926959@ds145369.mlab.com:45369/sky-school");
+// mongoose.connection.once('open', function(){
+//     console.log("server connected");
+// })
+
+// app.listen(port, function(){
+//     console.log("app is runnig on port", 3000);
+// });
+
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
 
-var port = (process.env.PORT || 3000);
+app.use(bodyParser.json())
+app.use(cors());
+var studentSchema = new mongoose.Schema({
+    name: String
+})
 
-app.get("/", function(req, res, next){
-    console.log("request is comming to '/");
-    res.send("hello Word");
-});
-app.get("/home", function(req, res, next){
-    console.log("request home is comming to '/");
-    res.send("this is my home page..");
+var studentModel = mongoose.model("students", studentSchema);
+
+app.get("/sub", function (req, res, next) {
+    studentModel.find({}, function (err, data) {
+        if (!err) {
+            console.log(data)
+            res.send(data)
+        }
+        else {
+            console.log("Error")
+        }
+    })
+
+})
+app.post("/sub",function(req,res,next)
+{
+    var newAdd = new studentModel({
+        name: req.body.name
+    })
+    newAdd.save(function(err,data)
+    {
+        if(!err)
+        {
+            console.log("Saved");
+        }
+        else
+        {
+            console.log("Error");
+        }
+    });
+})
+var port = (process.env.PORT || 3000)
+mongoose.connect('mongodb://aayanqazi:xp9999955555@ds151059.mlab.com:51059/hello-world')
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+    console.log("Connected");
 });
 
-app.listen(port, function(){
-    console.log("app is runnig on port", 3000);
+
+app.listen(port, () => {
+    console.log("Server Running At http://localhost:" + port);
 });
